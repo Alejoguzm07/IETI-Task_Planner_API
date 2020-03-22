352b45b35a2d76927757ddc6a1ea506fec0c84b0
Alejoguzm07 <alejoguzm07@gmail.com>
2020-03-22 15:58:34 -0500

    Sampple Task

--- src/main/java/edu/eci/taskplanner/service/impl/TaskServiceImpl.java
+++ src/main/java/edu/eci/taskplanner/service/impl/TaskServiceImpl.java
@@ -1,18 +1,34 @@
 package edu.eci.taskplanner.service.impl;
 
+import edu.eci.taskplanner.model.Status;
 import edu.eci.taskplanner.model.Task;
 import edu.eci.taskplanner.model.User;
 import edu.eci.taskplanner.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
 	@Override public List<Task> getAll() {
-
-		return null;
+		User user = User.builder()
+				.email("admin")
+				.fullname("administrador")
+				.password("admin")
+				.build();
+		Task task = Task.builder()
+				.description("Test")
+				.dueDate(new Date(Calendar.getInstance().getTime().getTime()))
+				.status(Status.IN_PROGRESS)
+				.user(user)
+				.build();
+		ArrayList<Task> tasks = new ArrayList<>();
+		tasks.add(task);
+		return tasks;
 	}
 
 	@Override public Task getById(String id) {
